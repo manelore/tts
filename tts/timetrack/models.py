30f528f63408104feb481fb5e84a246434ce00e8
Andrei Zhlobich <a.zhlobich@gmail.com>
2013-04-14 22:26:25 +0300

    Populate models WorkLog & WorkLogHistory

--- tts/timetrack/models.py
+++ tts/timetrack/models.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, division, unicode_literals, absolute_import
 
 from django.db import models
 from django.conf import settings
@@ -7,22 +8,38 @@ USER_MODEL = settings.AUTH_USER_MODEL
 
 
 class WorkType(models.Model):
+
     name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Work type"
+
+    def __str__(self):
+        return "Work type {0}".format(self.name)
 
 
 class Project(models.Model):
+
     users = models.ManyToManyField(USER_MODEL)
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Project"
+
 
 class Task(models.Model):
+
     description = models.CharField(max_length=500)
-    estimate = models.PositiveIntegerField()
+    estimate = models.IntegerField()
     deadline = models.DateTimeField()
     project = models.ForeignKey(Project)
 
+    class Meta:
+        verbose_name = "Task"
 
-class WorkLogFieldsMixin(object):
+
+class WorkLogFieldsMixin(models.Model):
+
     task = models.ForeignKey(Task)
     work_type = models.ForeignKey(WorkType)
     user = models.ForeignKey(USER_MODEL)
@@ -30,20 +47,37 @@ class WorkLogFieldsMixin(object):
     finish_at = models.DateTimeField()
     active = models.BooleanField(null=False, default=True, db_index=True)
 
+    class Meta:
+        abstract = True
 
-class WorkLog(models.Model, WorkLogFieldsMixin):
-    pass
+
+class WorkLog(WorkLogFieldsMixin, models.Model):
+
+    class Meta:
+        verbose_name = "Work log"
 
 
-class WorkLogHistory(models.Model, WorkLogFieldsMixin):
+class WorkLogHistory(WorkLogFieldsMixin, models.Model):
+
     parent_worklog = models.ForeignKey(WorkLog)
+
+    class Meta:
+        verbose_name = "Work log history"
 
 
 class Request(models.Model):
+
     user = models.ForeignKey(USER_MODEL)
+
+    class Meta:
+        verbose_name = "Request"
 
 
 class DayOffLog(models.Model):
+
     user = models.ForeignKey(USER_MODEL)
     start_at = models.DateTimeField()
     finish_at = models.DateTimeField()
+
+    class Meta:
+        verbose_name = "Day off"
