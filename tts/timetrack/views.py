ea27e53f7de070f0b2ca0498d02d4025f671a597
Andrei Zhlobich <a.zhlobich@gmail.com>
2013-04-26 02:01:49 +0300

    Add simple filter

--- tts/timetrack/views.py
+++ tts/timetrack/views.py
@@ -1,28 +1,50 @@
 from django.shortcuts import render
 from django import forms
 
-from timetrack.models import TaskWithTime
+from timetrack.models import *  # noqa
 
 
 class FilterForm(forms.Form):
-	start_date = forms.DateTimeField()
-	end_date = forms.DateTimeField()
-	ch_deadline = forms.DateTimeField()
-	ch_project = forms.CharField()
+    start_date = forms.DateTimeField(required=False)
+    end_date = forms.DateTimeField(required=False)
+    project = forms.ModelChoiceField(
+        required=False,
+        queryset=Project.objects.all(),
+        empty_label="Any project",
+    )
+    min_estimate = forms.IntegerField(min_value=0, required=False)
+    max_estimate = forms.IntegerField(min_value=0, max_value=99999, required=False)
 
 
 def tasks(request):
 
-	ff = FilterForm(request.GET)
-	if not ff.is_vaild():
-		return "Invalid form"
+    ff = FilterForm(request.GET)
 
-	tasks = TaskWithTime.objects.all()
+    tasks = TaskWithTime.objects.all()
+    if ff.is_valid():
 
-	return render(
-		request,
-		'timetrack/task_list.html',
-		{'tasks': tasks})
+        start_date = ff.cleaned_data['start_date']
+        end_date = ff.cleaned_data['end_date']
+        project = ff.cleaned_data['project']
+        min_estimate = ff.cleaned_data['min_estimate']
+        max_estimate = ff.cleaned_data['max_estimate']
+
+        if start_date:
+            tasks = tasks.filter(deadline__gte=start_date)
+        if end_date:
+            tasks = tasks.filter(deadline__lte=end_date)
+        if project:
+            tasks = tasks.filter(project=project)
+        if min_estimate:
+            tasks = tasks.filter(estimate__gte=min_estimate)
+        if max_estimate:
+            tasks = tasks.filter(estimate__lte=max_estimate)
+
+    return render(
+        request,
+        'timetrack/task_list.html',
+        {'tasks': tasks, 'fform': ff},
+    )
 
 
 def index(request):
